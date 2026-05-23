99aa2839232506d2801267022d91882e994c84f3
ngichmanadimike-commits <ngichmanadimike@gmail.com>
2026-05-23 22:26:52 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import "./App.css";
+import "./index.css";
 import ErrorBoundary from "@/components/ErrorBoundary";
 
 createRoot(document.getElementById("root")!).render(
