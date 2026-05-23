046dff1129ff4b28999a4388637aa746fa3023be
ngichmanadimike-commits <ngichmanadimike@gmail.com>
2026-05-23 19:24:36 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    chunkSizeWarningLimit: 1000,
+  server: {
+    port: 5173,
   },
-}));
+});
